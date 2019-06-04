676daa8593ea6f889e76b45bddff33fb29953110
mits58 <nakano@art.ist.hokudai.ac.jp>
2019-06-04 19:58:48 +0900

    added factorize method

--- Number_Theory/NumTh.java
+++ Number_Theory/NumTh.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 class NumTh{
@@ -20,18 +21,39 @@ class NumTh{
 		else if(x % 2 == 0) return pow((a * a) % m, x / 2, m) % m;
 		else return (a * pow((a * a) % m, x / 2, m)) % m;
 	}
+	static boolean ip(long x) {
+		if(x == 1) return false;
+		for(long i = 2; i * i <= x; i++) if(x % i == 0) return false;
+		return true;
+	}
+	static LinkedList<Long> fact(long n) {
+		long tn = n;
+		LinkedList<Long> factorize = new LinkedList<Long>();
+		if(ip(n)) {
+			factorize.add(n);
+			n /= n;
+		}
+		for(long i = 2; i <= tn; i++) {
+			if(n == 1) break;
+			if(ip(i)) {
+				while(n % i == 0) {
+					factorize.add(i);
+					n /= i;
+				}
+			}
+		}
+		return factorize;
+	}
 }
 public class Main{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		while(sc.hasNext()){
-			int n = sc.nextInt();
-			int ans = 1;
-			for(int i = 0; i < n; i++) {
-				int a = sc.nextInt();
-				ans = NumTh.LCM(ans, a);
-			}
-			System.out.println(ans);
+			long n = sc.nextLong();
+			LinkedList<Long> fact = NumTh.fact(n);
+			System.out.print(n+":");
+			for(long num : fact) System.out.print(" "+num);
+			System.out.println();
 		}
 	}
 }
