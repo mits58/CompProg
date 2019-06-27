59f46eabc1f813b5e0766ff37613bbfa89b7aa28
mits58 <ynakano927@gmail.com>
2019-06-27 14:14:54 +0900

    add some algorithms for tree

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
+
+class UnionFind{
+	int[] par;
+	UnionFind(int n){
+		par=new int[n];
+		for(int i=0;i<n;i++)par[i]=i;
+	}
+	public int find(int x){
+		if(par[x]==x)return x;
+		return par[x]=find(par[x]);
+	}
+	public Boolean same(int x,int y){
+		return find(x)==find(y);
+	}
+	public void unite(int x,int y){
+		if(find(x)==find(y))return;
+		par[find(x)]=find(y);
+	}
+}
 
 class State implements Comparable<State>{
 	int v, cost;
@@ -12,12 +33,17 @@ class State implements Comparable<State>{
 		return cost - s.cost;
 	}
 }
-class Edge{
+
+class Edge implements Comparable<Edge>{
 	int s, t, w;
 	public Edge(int S, int T, int W) {
 		s = S; t = T; w = W;
 	}
+	public int compareTo(Edge e) {
+		return w - e.w;
+	}
 }
+
 class Graph{
 	static int INF = Integer.MAX_VALUE/2; // INF 問題により変えよう
 	private int V, E;
@@ -103,31 +129,70 @@ class Graph{
 		}
 		return res;
 	}
+
+	/* Calculates of Minimum Spanning Tree and returns the cost using kruskal algorithm */
+	public int calc_MST() {
+		ArrayList<Edge> alledges = new ArrayList<Edge>();
+		for(int i = 0; i < V; i++) {
+			for(Edge e : edges[i]) alledges.add(e);
+		}
+		Collections.sort(alledges);
+		UnionFind uf = new UnionFind(V);
+		int sum = 0;
+		for(Edge e : alledges) {
+			if(!uf.same(e.s, e.t)) {
+				// Using edge e
+				sum += e.w;
+				uf.unite(e.s, e.t);
+			}
+		}
+		return sum;
+	}
+
+	/* Calculates of diameter of tree using dijkstra */
+	public int calc_Diameter() {
+		int[] dist = dijkstra(0);
+		int max=0;
+		for(int i = 1; i < V; i++) if(dist[max] < dist[i]) max = i;
+		dist = dijkstra(max);
+		int res = 0;
+		for(int i = 0; i < V; i++) res = Math.max(res, dist[i]);
+		return res;
+	}
+
+	/* Calculates of height of each node of the tree */
+	public int[] calc_Height() {
+		int[] ans = new int[V];
+		int[] d = dijkstra(0);
+		// Calculates farthest pair
+		int first = 0;
+		for(int i = 1; i < V; i++) if(d[i] > d[first]) first = i;
+		int[] dist_first = dijkstra(first);
+		int second = 0;
+		for(int i = 1; i < V; i++) if(dist_first[i] > dist_first[second]) second = i;
+		int[] dist_second = dijkstra(second);
+
+		for(int i = 0; i < V; i++) {
+			ans[i] = Math.max(dist_first[i], dist_second[i]);
+		}
+		return ans;
+	}
 }
 class Main{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		while(sc.hasNext()){
-			int V = sc.nextInt(), E = sc.nextInt();
-			Graph g = new Graph(V, E);
-			for(int i = 0; i < E; i++) {
+			int n = sc.nextInt();
+			Graph g = new Graph(n, n - 1);
+			for(int i = 0; i < n - 1; i++) {
 				int s = sc.nextInt();
 				int t = sc.nextInt();
 				int d = sc.nextInt();
 				g.edges[s].add(new Edge(s, t, d));
+				g.edges[t].add(new Edge(t, s, d));
 			}
-			int[][] ans = g.FloydWarshall();
-			if(ans == null) {
-				System.out.println("NEGATIVE CYCLE");
-			}else {
-				for(int i = 0; i < V; i++) {
-					System.out.print((ans[i][0] == Graph.INF) ? "INF" : ans[i][0]);
-					for(int j = 1; j < V; j++) {
-						System.out.print((ans[i][j] == Graph.INF) ? " INF" : " "+ans[i][j]);
-					}
-					System.out.println();
-				}
-			}
+			int q = sc.nextInt();
+
 		}
 	}
 }
